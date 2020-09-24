846df2aae7d3d8db5fe6ec2f7f28382fd55c3822
Patryk Woziński <patryk.wozinski@gmail.com>
2020-09-24 15:47:31 +0200

    simplify

--- lib/sheet_service/detector/always_utf8_detector.ex
+++
@@ -1,8 +0,0 @@
-defmodule SheetService.Detector.AlwaysUtf8 do
-  @moduledoc false
-  @behaviour SheetService.Detector
-
-  def detect_encoding(file_path) do
-    {:ok, file_path, "utf-8"}
-  end
-end
--- test/sheet_service/detector/always_utf8_detector_test.exs
+++
@@ -1,7 +0,0 @@
-defmodule SheetServiceTest.Detector.AlwaysUtf8Test do
-  use ExUnit.Case
-
-  test "it is always utf-8" do
-    assert {:ok, nil, "utf-8"} == SheetService.Detector.AlwaysUtf8.detect_encoding(nil)
-  end
-end
