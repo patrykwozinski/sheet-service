c120e0ea9cbffd0d61fdcdea483b85a80592ed99
Patryk Woziński <patryk.wozinski@gmail.com>
2020-09-24 11:28:50 +0200

    cleanup

--- lib/sheet_service/config.ex
+++ lib/sheet_service/config.ex
@@ -1,4 +1,5 @@
 defmodule SheetService.Config do
+  @spec get(String.t()) :: SheetService.Detector.Dummy
   def get(_name) do
     SheetService.Detector.Dummy
   end
--- lib/sheet_service/detector/dummy_detector.ex
+++ lib/sheet_service/detector/dummy_detector.ex
@@ -2,6 +2,7 @@ defmodule SheetService.Detector.Dummy do
   @moduledoc false
   @behaviour SheetService.Detector
 
+  @spec detect_encoding(File.t()) :: {:ok, String.t()}
   def detect_encoding(_file) do
     {:ok, "utf-8"}
   end
