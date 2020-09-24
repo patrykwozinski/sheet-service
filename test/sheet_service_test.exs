90498991cbd81c90b80abd0649125837ff88c13a
Patryk Woziński <patryk.wozinski@gmail.com>
2020-09-24 16:00:48 +0200

    some detail

--- test/sheet_service_test.exs
+++ test/sheet_service_test.exs
@@ -2,7 +2,7 @@ defmodule SheetServiceTest do
   use ExUnit.Case
   doctest SheetService
 
-  test "process given file" do
+  test "process given csv file" do
     {result, _message} = SheetService.process("test/fixtures/simple.csv")
 
     assert result == :ok
