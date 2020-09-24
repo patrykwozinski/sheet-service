e6b78fe48f68ef2d159db8040e175aee3aceefcb
Patryk Woziński <patryk.wozinski@gmail.com>
2020-09-24 13:15:09 +0200

    rename -> encoding_validate

--- lib/sheet_service.ex
+++ lib/sheet_service.ex
@@ -14,13 +14,17 @@ defmodule SheetService do
   def process do
     "simple.csv"
     |> SheetService.Detector.detect_encoding()
-    |> encoding_supported()
+    |> encoding_validate()
     |> SheetService.Parser.parse()
 
     {:ok, "Sheet successfully processed"}
   end
 
-  defp encoding_supported({:ok, file_path, "utf-8"}) do
+  defp encoding_validate({:ok, file_path, "utf-8"}) do
+    file_path
+  end
+
+  defp encoding_validate(:ok, file_path, _) do
     file_path
   end
 end
