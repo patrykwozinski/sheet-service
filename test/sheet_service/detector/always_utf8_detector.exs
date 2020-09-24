844748e4e1491cd0843849fe7c23439037d546af
Patryk Woziński <patryk.wozinski@gmail.com>
2020-09-24 11:49:22 +0200

    fixed test

--- test/sheet_service/detector/always_utf8_detector.exs
+++
@@ -1,7 +0,0 @@
-defmodule SheetService.Detector.AlwaysUtf8Test do
-  use ExUnit.Case
-
-  test "it is always utf-8" do
-    assert SheetService.Detector.AlwaysUtf8.detect_encoding(nil) == "utf-8"
-  end
-end
