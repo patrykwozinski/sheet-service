8bcea59174d799af8779f61b4d7186f8392c911c
Patryk Woziński <patryk.wozinski@gmail.com>
2020-09-24 11:47:39 +0200

    spec for any

--- lib/sheet_service/detector/always_utf8_detector.ex
+++ lib/sheet_service/detector/always_utf8_detector.ex
@@ -2,7 +2,7 @@ defmodule SheetService.Detector.AlwaysUtf8 do
   @moduledoc false
   @behaviour SheetService.Detector
 
-  @spec detect_encoding(File.t()) :: {:ok, String.t()}
+  @spec detect_encoding(any) :: {:ok, String.t()}
   def detect_encoding(_file) do
     {:ok, "utf-8"}
   end
