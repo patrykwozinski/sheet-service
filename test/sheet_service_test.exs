01b82baf966d889882a76831530293bde93ead6e
Patryk Woziński <patryk.wozinski@gmail.com>
2020-09-28 11:33:36 +0200

    first and last

--- test/sheet_service_test.exs
+++ test/sheet_service_test.exs
@@ -4,13 +4,17 @@ defmodule SheetServiceTest do
   describe "positive template 1" do
     test "csv processed" do
       assert {:ok, data} = SheetService.process(fixture_positive("tmpl1/CSV.csv"))
+      assert length(data) == 3
 
       first_result = data |> List.first()
-
-      assert length(data) == 3
       assert first_result["email"] == "andreas@email.de"
       assert first_result["first_name"] == "Andrea§"
       assert first_result["last_name"] == "Fischer"
+
+      last_result = data |> List.last()
+      assert last_result["email"] == "emilia.schnšider@gmail.com"
+      assert last_result["first_name"] == "Emilia"
+      assert last_result["last_name"] == "Schneider"
     end
   end
 
