979943bd97ff4e7d32bfa9345cc451c1a3c30256
Patryk Woziński <patryk.wozinski@gmail.com>
2020-09-28 11:23:35 +0200

    test imrpvoded

--- test/sheet_service_test.exs
+++ test/sheet_service_test.exs
@@ -4,6 +4,13 @@ defmodule SheetServiceTest do
   describe "positive template 1" do
     test "csv processed" do
       assert {:ok, data} = SheetService.process(fixture_positive("tmpl1/CSV.csv"))
+
+      first_result = data |> List.first()
+
+      assert length(data) == 3
+      assert first_result["email"] == "andreas@email.de"
+      assert first_result["first_name"] == "Andrea§"
+      assert first_result["last_name"] == "Fischer"
     end
   end
 
